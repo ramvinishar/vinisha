792cfe80e2b9969943a759bdea34fb9f207c6615
Ram Vinisha <ramvinishar.ug22.it@francisxavier.ac.in>
2025-02-05 12:44:11 +0530

    server

--- server.js
+++ server.js
@@ -148,6 +148,6 @@ app.post('/saveLabDetails', upload.single('resume'), async (req, res) => {
 });
 
 // Start Server
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 1000;
 app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
 
